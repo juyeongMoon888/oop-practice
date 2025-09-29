ed7901099751c3d952c01fdd5dc19878f9a7bb01
Juyeong Moon <juyeongMoon888@gmail.com>
2025-09-29 22:50:55 +0900

    init commit

---
+++ src/main/java/org/inheritance/Car.java
@@ -0,0 +1,12 @@
+package org.inheritance;
+
+public class Car {
+
+    public void drive() {
+        System.out.println("운전합니다.");
+    }
+
+    public void fillUp() {
+        System.out.println("주유합니다.");
+    }
+}
