4eab1b5800caa8959644594df82cac19b8ba051e
Juyeong Moon <juyeongMoon888@gmail.com>
2025-09-30 15:01:06 +0900

    refactor: Car 클래스에서 불필요한 메서드 제거 후 move()만 유지

--- src/main/java/org/inheritance/Car.java
+++ src/main/java/org/inheritance/Car.java
@@ -2,11 +2,7 @@ package org.inheritance;
 
 public class Car {
 
-    public void drive() {
-        System.out.println("운전합니다.");
-    }
-
-    public void fillUp() {
-        System.out.println("주유합니다.");
+    public void move() {
+        System.out.println("차가 이동합니다.");
     }
 }
