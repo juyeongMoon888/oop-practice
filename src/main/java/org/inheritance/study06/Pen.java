72d469344e0979a02e065a24a90bea3843ec2017
Juyeong Moon <whoor2828@gmail.com>
2025-10-05 14:38:51 +0900

    feat: 슈퍼 클래스 Pen 생성

---
+++ src/main/java/org/inheritance/study06/Pen.java
@@ -0,0 +1,30 @@
+package org.inheritance.study06;
+
+public class Pen {
+    private int width;
+    private int amount;
+    private String color;
+
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
