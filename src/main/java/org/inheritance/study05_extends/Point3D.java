800f13c0a15eca355f7e268935a73f13f52d59f6
Juyeong Moon <whoor2828@gmail.com>
2025-10-17 23:31:00 +0900

    feat: Point3D 클래스 추가

---
+++ src/main/java/org/inheritance/study05_extends/Point3D.java
@@ -0,0 +1,18 @@
+package org.inheritance.study05_extends;
+
+public class Point3D extends Point{
+    int z = 0;
+    Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public void moveUp(int n) {
+        z = n-z;
+    }
+
+    @Override
+    public String toString() {
+        return "("+super.getX()+","+super.getY()+","+z+")의 점";
+    }
+}
