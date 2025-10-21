5680633ed2f585a875d45f5728a726b7462c84f3
Juyeong Moon <whoor2828@gmail.com>
2025-10-21 18:38:40 +0900

    feat: Box 클래스 추가

--- src/main/java/org/inheritance/study05_extends/coffeeMachine/Box.java
+++ src/main/java/org/inheritance/study05_extends/coffeeMachine/Box.java
@@ -1,5 +1,13 @@
-package org.inheritance.study05_extends.coffeMachine;
+package org.inheritance.study05_extends.coffeeMachine;
 
 abstract class Box {
-
+    protected int size;
+    public Box(int size) { //현재 박스에 들어 있는 재료의 양
+        this.size = size; // 생성자
+    }
+    public boolean isEmpty(){
+        return size == 0; // 박스가 빈 경우 true 리턴
+    }
+    public abstract boolean consume(); // 박스에 들어 있는 재료를 일정량 소비
+    public abstract void print(); // 박스에 들어 있는 링을 "*" 문자로 출력
 }
