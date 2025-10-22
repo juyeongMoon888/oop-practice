4fe3e4e6ae1104ebd94f94c070dae555f5af1705
Juyeong Moon <whoor2828@gmail.com>
2025-10-22 22:55:08 +0900

    feat: Application(main)추가

---
+++ src/main/java/org/inheritance/study05_extends/coffeeMachine/Application.java
@@ -0,0 +1,7 @@
+package org.inheritance.study05_extends.coffeeMachine;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("*****청춘 커피 자판기 입니다.*****");
+    }
+}
