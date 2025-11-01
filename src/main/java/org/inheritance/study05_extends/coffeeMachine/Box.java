9e220de2a73323dbce0aa7c8481ec507fde6bd7d
Juyeong Moon <whoor2828@gmail.com>
2025-11-01 22:12:18 +0900

    feat: getSize()추가

--- src/main/java/org/inheritance/study05_extends/coffeeMachine/Box.java
+++ src/main/java/org/inheritance/study05_extends/coffeeMachine/Box.java
@@ -8,6 +8,11 @@ abstract class Box {
     public boolean isEmpty(){
         return size == 0; // 박스가 빈 경우 true 리턴
     }
+
+    public int getSize() {
+        return size;
+    }
+
     public abstract boolean consume(); // 박스에 들어 있는 재료를 일정량 소비
     public abstract void print(); // 박스에 들어 있는 링을 "*" 문자로 출력
 }
