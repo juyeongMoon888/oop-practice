af78dc3b8875f678ad3e5e107e5c4b378dec0c72
Juyeong Moon <whoor2828@gmail.com>
2025-11-01 23:20:34 +0900

    feat: IngredientBod consume/print 구현 및 getName·setSize 추가

--- src/main/java/org/inheritance/study05_extends/coffeeMachine/IngredientBox.java
+++ src/main/java/org/inheritance/study05_extends/coffeeMachine/IngredientBox.java
@@ -9,11 +9,26 @@ public class IngredientBox extends Box{
 
     @Override
     public boolean consume() {
+        if (super.size > 0) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public void print() {
-        super.getSize();
+        for (int i = 0; i < super.size; i++) {
+            System.out.print("*");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setSize(int size) {
+        if (consume()) {
+            super.size -= size;
+        }
     }
 }
